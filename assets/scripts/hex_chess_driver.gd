c9d2f1891541bfeb384bb7c0ea93390a81f74114
Cory Johns <cory@triplevisiongroup.com>
2025-06-01 12:17:37 -0700

    Continued work on gdscript-to-cpp driver code:
    - most functions are implemented
    - hex_chess_driver.gd script will manage pieces and highlights on the
      board. The board tiles will be placed below the driver script in the
      hierarchy. When the player clicks on a tile a tile clicked event will
      trigger and activate the OnTileClicked function in the driver. After
      the current player has committed to a move (by clicking on a tile
      containing a move action), the game will progress to the next player.
    - TODO add tile management gdscript
    - TODO need to add functions to add/remove pieces and highlights
    - TODO need to add game loop
    - TODO add user interface
    - TODO connect OnTileClicked function to a tile clicked signal

--- assets/scripts/hex_chess_driver.gd
+++ assets/scripts/hex_chess_driver.gd
@@ -2,6 +2,23 @@ extends HexChessDriver
 
 enum ActionType {NoAction, Selectable, Move, MoveAndSelect}
 enum PieceType {King, Queen, Rook, Bishop, Knight, Pawn, NoPiece}
+enum GameState {Running, Checkmate, Stalemate, DeadPosistion, ThreefoldRepitition, FiftyMoveRule}
+var WhitePieces = {
+	PieceType.King:"res://assets/scenes/pieces/King-White.tscn",
+	PieceType.Queen:"res://assets/scenes/pieces/Queen-White.tscn",
+	PieceType.Rook:"res://assets/scenes/pieces/Rook-White.tscn",
+	PieceType.Bishop:"res://assets/scenes/pieces/Bishop-White.tscn",
+	PieceType.Knight:"res://assets/scenes/pieces/Knight-White.tscn",
+	PieceType.Pawn:"res://assets/scenes/pieces/Pawn-White.tscn"
+	}
+var BlackPieces = {
+	PieceType.King:"res://assets/scenes/pieces/King-Black.tscn",
+	PieceType.Queen:"res://assets/scenes/pieces/Queen-Black.tscn",
+	PieceType.Rook:"res://assets/scenes/pieces/Rook-Black.tscn",
+	PieceType.Bishop:"res://assets/scenes/pieces/Bishop-Black.tscn",
+	PieceType.Knight:"res://assets/scenes/pieces/Knight-Black.tscn",
+	PieceType.Pawn:"res://assets/scenes/pieces/Pawn-Black.tscn"
+	}
 
 class ChessPiece:
 	var player
@@ -11,25 +28,62 @@ class ChessPiece:
 		type = _type
 
 func _ready() -> void:
-	pass
-	
+	RoundSetup() # declare a new round
+	GetSelectableTiles()
+	UpdateBoard()
+
+func ReturnGameState() -> GameState:
+	var value:int = GetGameState()
+	return (value + 1) as GameState
+
 func UpdateBoard() -> void:
 	var x:int = 6
 	var s:int = 0
 	for i in 11:
 		for j in x:
 			# get board state at coordinates
-			var piece:ChessPiece = ParsePieceType(GetPieceOnTile(i, j))
-			# TODO update piece on board at location
-			var action:ActionType = ParseActionType(GetActionOnTile(i, j))
-			# TODO update tile highlight at location
-			
+			var piece:ChessPiece = ParsePieceType(GetPieceOnTile(i, j + s))
+			# update piece on board at location
+			SetPieceObjectOnTile(piece, i, j+s)
+			var action:ActionType = ParseActionType(GetActionOnTile(i, j + s))
+			# update tile highlight at location
+			SetTileHighlight(action, i, j+s)
+		# files grow by one on the end before rank 5,
+		# and shrink by one at the start after rank 5.
 		if i >= 5:
 			x -= 1
 			s += 1
 		else:
 			x += 1
-	
+
+func GetCurrentPieceObjectOnTile(rank:int, file:int) -> ChessPiece:
+	# TODO lookup piece on game board
+	return ChessPiece.new(true, PieceType.King)
+
+func SetPieceObjectOnTile(piece:ChessPiece, rank:int, file:int) -> void:
+	if piece == GetCurrentPieceObjectOnTile(rank, file):
+		return # skip if no change
+	if piece.player:
+		# white player
+		var piece_list = WhitePieces
+	else:
+		# black player
+		var piec_list = BlackPieces
+	# clear piece on tile
+	# TODO
+	# set new piece on tile
+	# TODO
+
+func SetTileHighlight(action:ActionType, rank:int, file:int) -> void:
+	# TODO clear Highlight
+	# TODO set new Highlight
+	pass
+
+func ClearCurrentSelection() -> void:
+	ClearSelection() # remove current selection
+	GetSelectableTiles() # update selectable tiles
+	UpdateBoard()
+
 func OnTileClicked(rank:int, file:int) -> void:
 	# verifiy coordinates are within board range
 	if rank > 10 or rank < 0:
@@ -42,12 +96,18 @@ func OnTileClicked(rank:int, file:int) -> void:
 		return
 	elif action == ActionType.Selectable:
 		# update tiles with moves for selectable
-		GetMoveTiles(rank, file)
+		GetMoveTiles(rank, file) # notify driver of piece selection
+		UpdateBoard()
 	elif action == ActionType.Move:
-		pass
+		# notify driver of move
+		MovePiece(rank, file)
+		UpdateBoard()
+		RoundCleanup() # round over, setup for next round
 	else:
-		pass
-	
+		# tile has both a selection and a move
+		# TODO
+		print("NOT IMPLEMENTED :: Tile has both selection and move types!")
+
 func ParsePieceType(piece:int) -> ChessPiece:
 	if piece == 0:
 		# no piece
@@ -58,7 +118,7 @@ func ParsePieceType(piece:int) -> ChessPiece:
 	else:
 		# black piece
 		return ChessPiece.new(false, ParsePieceTypeNoPlayer(piece - 7))
-		
+
 func ParsePieceTypeNoPlayer(piece:int) -> PieceType:
 	match piece:
 		0:
@@ -75,7 +135,7 @@ func ParsePieceTypeNoPlayer(piece:int) -> PieceType:
 			return PieceType.Pawn
 		_:
 			return PieceType.NoPiece
-	
+
 func ParseActionType(action:int) -> ActionType:
 	match action:
 		0:
